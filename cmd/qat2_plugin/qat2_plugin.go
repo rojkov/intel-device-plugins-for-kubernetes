e936b0865f38ad6e5645011f7aeac1be5612ce9a
Dmitry Rozhkov <dmitry.rozhkov@linux.intel.com>
2018-11-07 12:32:03 +0200

    WIP

--- cmd/qat2_plugin/qat2_plugin.go
+++ cmd/qat2_plugin/qat2_plugin.go
@@ -18,6 +18,7 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
 
 	"github.com/Thomasdezeeuw/ini"
@@ -25,6 +26,18 @@ import (
 
 	"github.com/intel/intel-device-plugins-for-kubernetes/pkg/debug"
 )
+
+type endpoint struct {
+	id        string
+	processes int
+}
+
+type section struct {
+	endpoints          []endpoint
+	cryptoEngines      int
+	compressionEngines int
+	pinned             bool
+}
 
 func main() {
 	debugEnabled := flag.Bool("debug", false, "enable debug output")
@@ -43,6 +56,7 @@ func main() {
 	}
 	output := string(outputBytes[:])
 
+	driverConfig := make(map[string]section)
 	for ln, line := range strings.Split(output, "\n") {
 		if !strings.HasPrefix(line, " qat_") {
 			continue
@@ -79,11 +93,69 @@ func main() {
 			panic(err)
 		}
 
-		fmt.Println(ln, devID, line)
+		debug.Print(ln, devID, line)
 
-		for k, v := range config["SHIM"] {
-			fmt.Println(k, v)
+		for sectionName, data := range config {
+			if sectionName == "GENERAL" || sectionName == "KERNEL" || sectionName == "" {
+				continue
+			}
+			debug.Print(sectionName)
+
+			numProcesses, err := strconv.Atoi(data["NumProcesses"])
+			if err != nil {
+				panic(err)
+			}
+			cryptoEngines, err := strconv.Atoi(data["NumberCyInstances"])
+			if err != nil {
+				panic(err)
+			}
+			compressionEngines, err := strconv.Atoi(data["NumberDcInstances"])
+			if err != nil {
+				panic(err)
+			}
+			pinned := false
+			if limitDevAccess, ok := data["LimitDevAccess"]; ok {
+				if limitDevAccess != "0" {
+					pinned = true
+				}
+			}
+
+			if old, ok := driverConfig[sectionName]; ok {
+				// first check the sections are consistent across endpoints
+				if old.pinned != pinned {
+					fmt.Println("ERROR: the value of LimitDevAccess must be consistent across all devices in", sectionName)
+					os.Exit(1)
+				}
+				if !pinned && old.endpoints[0].processes != numProcesses {
+					fmt.Println("ERROR: for not pinned sections NumProcesses must be equal for all devices. Error in", sectionName)
+					os.Exit(1)
+				}
+				if old.cryptoEngines != cryptoEngines || old.compressionEngines != compressionEngines {
+					fmt.Println("ERROR: NumberCyInstances and NumberDcInstances must be consistent across all devices in", sectionName)
+					os.Exit(1)
+				}
+
+				// then add a new endpoint to the section
+				old.endpoints = append(old.endpoints, endpoint{
+					id:        devID,
+					processes: numProcesses,
+				})
+				driverConfig[sectionName] = old
+			} else {
+				driverConfig[sectionName] = section{
+					endpoints: []endpoint{
+						{
+							id:        devID,
+							processes: numProcesses,
+						},
+					},
+					cryptoEngines:      cryptoEngines,
+					compressionEngines: compressionEngines,
+					pinned:             pinned,
+				}
+			}
 		}
 
 	}
+	debug.Print(driverConfig)
 }
